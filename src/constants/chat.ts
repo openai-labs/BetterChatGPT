8830d4838473ea12363506676e242801f0196f06
myWsq <wsq961@outlook.com>
2023-04-16 15:04:35 +0800

    remove default system message

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -16,8 +16,8 @@ export const _defaultSystemMessage =
 
 export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
-  'gpt-4',
-  'gpt-4-32k',
+  // 'gpt-4',
+  // 'gpt-4-32k',
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
   // 'gpt-4-32k-0314',
